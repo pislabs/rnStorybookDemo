bb2cc6b6186d9c9e4135996f54f9641062e44d3b
rayl <liuyi86@zto.com>
2024-12-23 16:06:23 +0800

    feat: 支持state

--- components/TaskList.stories.jsx
+++ components/TaskList.stories.jsx
@@ -2,6 +2,48 @@
 import { TaskList } from './TaskList';
 import { Default as TaskStory } from './Task.stories';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+// A super-simple mock of the state of the store
+const MockedState = {
+  tasks: [
+    { ...TaskStory.args.task, id: '1', title: 'Task 1' },
+    { ...TaskStory.args.task, id: '2', title: 'Task 2' },
+    { ...TaskStory.args.task, id: '3', title: 'Task 3' },
+    { ...TaskStory.args.task, id: '4', title: 'Task 4' },
+    { ...TaskStory.args.task, id: '5', title: 'Task 5' },
+    { ...TaskStory.args.task, id: '6', title: 'Task 6' },
+  ],
+  status: 'idle',
+  error: null,
+};
+
+// A super-simple mock of a redux store
+const Mockstore = ({ taskboxState, children }) => (
+  <Provider
+    store={configureStore({
+      reducer: {
+        taskbox: createSlice({
+          name: 'taskbox',
+          initialState: taskboxState,
+          reducers: {
+            updateTaskState: (state, action) => {
+              const { id, newTaskState } = action.payload;
+              const task = state.tasks.findIndex((task) => task.id === id);
+              if (task >= 0) {
+                state.tasks[task].state = newTaskState;
+              }
+            },
+          },
+        }).reducer,
+      },
+    })}
+  >
+    {children}
+  </Provider>
+);
 
 export default {
   component: TaskList,
@@ -20,6 +62,9 @@ export default {
 };
 
 export const Default = {
+  decorators: [
+    (story) => <Mockstore taskboxState={MockedState}>{story()}</Mockstore>,
+  ],
   args: {
     // Shaping the stories through args composition.
     // The data was inherited from the Default story in Task.stories.js.
@@ -35,7 +80,28 @@ export const Default = {
 };
 
 export const WithPinnedTasks = {
+  decorators: [
+    (story) => {
+      const pinnedtasks = [
+        ...MockedState.tasks.slice(0, 5),
+        { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+      ];
+
+      return (
+        <Mockstore
+          taskboxState={{
+            ...MockedState,
+            tasks: pinnedtasks,
+          }}
+        >
+          {story()}
+        </Mockstore>
+      );
+    },
+  ],
   args: {
+    // Shaping the stories through args composition.
+    // Inherited data coming from the Default story.
     tasks: [
       ...Default.args.tasks.slice(0, 5),
       { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
@@ -44,6 +110,18 @@ export const WithPinnedTasks = {
 };
 
 export const Loading = {
+  decorators: [
+    (story) => (
+      <Mockstore
+        taskboxState={{
+          ...MockedState,
+          status: 'loading',
+        }}
+      >
+        {story()}
+      </Mockstore>
+    ),
+  ],
   args: {
     tasks: [],
     loading: true,
@@ -51,6 +129,18 @@ export const Loading = {
 };
 
 export const Empty = {
+  decorators: [
+    (story) => (
+      <Mockstore
+        taskboxState={{
+          ...MockedState,
+          tasks: [],
+        }}
+      >
+        {story()}
+      </Mockstore>
+    ),
+  ],
   args: {
     // Shaping the stories through args composition.
     // Inherited data coming from the Loading story.
